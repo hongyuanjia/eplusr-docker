5ae219ff2f1385b94d0b8df586b2db238676df6f
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2020-01-22 01:39:59 +0800

    Use build hooks.

---
+++ hooks/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+the Docker repository tag being built
+if [[ "$DOCKER_TAG" =~ "-verse" ]];
+then
+    docker build -t $IMAGE_NAME --build-arg UPSTREAM=verse .
+else
+    docker build -t $IMAGE_NAME .
+fi
+

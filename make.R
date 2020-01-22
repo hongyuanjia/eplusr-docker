7c0f905b15ec71a7bd2c86ada97580df5c85d24d
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2020-01-23 01:48:57 +0800

    Echo upstream when building.

--- make.R
+++ make.R
@@ -10,6 +10,9 @@ l <- paste0(
 ## Based on rocker/rstudio or rocker/verse Debian-based image
 FROM rocker/$UPSTREAM:latest
 
+ARG UPSTREAM
+RUN echo "Using upstream container rocker/$UPSTREAM:latest"
+
 LABEL org.label-schema.license="GPL-2.0" \\
       org.label-schema.vcs-url="https://github.com/hongyuanjia/eplusr-docker" \\
       org.label-schema.vendor="IDEAS Lab, NUS" \\

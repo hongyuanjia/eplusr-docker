0b68a4d20ee64d7dc1caa9538242c961ef57e8ab
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2020-01-23 21:49:43 +0800

    Tweak build hook.

--- hooks/build.sh
+++ hooks/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-if [[ $DOCKER_TAG =~ "-verse" ]];
+if [[ "$DOCKER_TAG" =~ "-verse" ]];
 then
+    echo "Building using upstream rocker/verse container..."
     docker build -t $IMAGE_NAME --build-arg UPSTREAM=verse .
 else
+    echo "Building using upstream rocker/rstudio container..."
     docker build -t $IMAGE_NAME .
 fi
 
